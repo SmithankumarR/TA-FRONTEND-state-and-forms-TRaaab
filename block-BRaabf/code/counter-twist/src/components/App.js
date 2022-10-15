4aae52eeeb6045bed0071bb0a4f51f26f31d9eda
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-10-15 19:07:48 +0530

    Counter App using React

--- block-BRaabf/code/counter-twist/src/components/App.js
+++ block-BRaabf/code/counter-twist/src/components/App.js
@@ -1,107 +1,57 @@
 import React from 'react';
+import '../stylesheets/App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      count: 0,
       step: 5,
-      counter: 0,
-      active: this.state.step,
+      max:15
     };
   }
-
-  handleSteps = () => {
-    if (this.state.step === 5) {
-      return this.state.step;
-    }
-    if (this.state.step === 10) {
-      return this.state.step + 5;
-    }
-    if (this.state.step === 15) {
-      return this.state.step + 10;
-    }
-
-    return this.state.step === this.state.active ? 'active' : 'btn-pri';
-  };
   handleIncrement = () => {
-    return this.state.counter + this.state.step;
+    let {step,max ,count} = this.state;
+    this.setState({
+      count: count + step > max ? count : count + step,
+    });
   };
   handleDecrement = () => {
-    return this.state.counter - this.state.step;
+    this.setState({
+      count: this.state.count - this.state.step,
+    });
   };
   handleReset = () => {
-    return this.state.counter;
+    this.setState({
+      count: 0,
+    });
   };
   render() {
     return (
-      <>
-        <div className="Counter flex">
-          <div className="steps">
-            <button
-              className="btn"
-              onClick={() => {
-                this.setState = {
-                  step: this.handleSteps,
-                };
-              }}
-            >
-              {this.state.step}
-            </button>
-            <button
-              className="btn"
-              onClick={() => {
-                this.setState = {
-                  step: this.handleSteps,
-                };
-              }}
-            >
-              {this.state.step}
-            </button>
-            <button
-              className="btn"
-              onClick={() => {
-                this.setState = {
-                  step: this.handleSteps,
-                };
-              }}
-            >
-              {this.state.step}
-            </button>
-            <div className="actions flex">
-              <button
-                className="btn-action"
-                onClick={() => {
-                  this.setState = {
-                    counter: this.handleIncrement,
-                  };
-                }}
-              >
-                Increment{' '}
-              </button>
-              <button
-                className="btn-action"
-                onClick={() => {
-                  this.setState = {
-                    counter: this.handleDecrement,
-                  };
-                }}
-              >
-                Increment{' '}
-              </button>
-              <button
-                className="btn-action"
-                onClick={() => {
-                  this.setState = {
-                    counter: this.handleReset,
-                  };
-                }}
-              >
-                Increment{' '}
-              </button>
-            </div>
+        <div className="container">
+          <h1>{this.state.count}</h1>
+          <h3>steps</h3>
+          <div className="steps flex btn">
+            {[5, 10, 15].map((step) => (
+              <button onClick={() => {
+                this.setState({step: step});
+              }} className={this.state.step === step ? 'active' : 'btn-pri'}>{step}</button>
+            ))}
+          </div>
+          <div className="steps max flex btn">
+          <h3>Max</h3>
+            {[15, 100, 200].map((max) => (
+              <button onClick={() => {
+                this.setState({step: max});
+              }} className={this.state.step === max ? 'active' : 'btn-pri'}>{max}</button>
+            ))}
+          </div>
+          <div className="actions flex">
+            <button onClick={this.handleIncrement}>Increment</button>
+            <button onClick={this.handleDecrement}>Decrement </button>
+            <button onClick={this.handleReset}>Reset</button>
           </div>
         </div>
-      </>
     );
   }
 }
