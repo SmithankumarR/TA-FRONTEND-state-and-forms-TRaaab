ae135f1b7602322dcc9062b530e3925a99bb40c8
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-10-17 20:32:17 +0530

    add: states

---
+++ block-BRaabi/code/faq/src/components/App.js
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function App() {
+  return (
+    <main className="container">
+      <h2 className="btn btn-pri">closed state</h2>
+      <Card />
+    </main>
+  );
+}
+
+export default App;
