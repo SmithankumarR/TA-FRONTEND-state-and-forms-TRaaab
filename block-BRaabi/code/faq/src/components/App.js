56ac70692281d0657023a0a5b0fb5506ef89e9bd
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-10-17 21:18:37 +0530

    Accordion Using React

--- block-BRaabi/code/faq/src/components/App.js
+++ block-BRaabi/code/faq/src/components/App.js
@@ -1,12 +1,39 @@
-import Card from "./Card";
+import React from "react";
+import faqs from "../data.json";
 
-function App() {
-  return (
-    <main className="container">
-      <h2 className="btn btn-pri">closed state</h2>
-      <Card />
-    </main>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: null,
+    };
+  }
+  render() {
+    return (
+      <main className="container">
+        <h1 className="heading">AltCampus FAQ</h1>
+        <ul>
+          {faqs.map((faq, index) => (
+            <li className="btn btn-sec">
+              <h2
+                onClick={() => {
+                  this.setState({
+                    activeIndex:
+                      this.state.activeIndex === index ? null : index,
+                  });
+                }}
+                className="question "
+              >
+                {faq.Q} {this.state.activeIndex === index ? "🔻" : "🔺"}
+              </h2>
+              {index === this.state.activeIndex && (
+                <p className="answer">{faq.A}</p>
+              )}
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
 }
-
 export default App;
