e8394a0f2f7ffe3576d72556788922162d99f580
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-10-25 15:18:42 +0530

    add : sizes

--- block-BRaabw/code/my_shopping_cart/src/index.js
+++ block-BRaabw/code/my_shopping_cart/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Stylesheets/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import './Stylesheets/index.css';
 import App from './Components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
